93fa9ce77bf558aae8f0519abc2fcd4861b19cbd
milton-reyes <altf4616@outlook.com>
2020-05-04 14:57:13 -0400

    changes

--- abstract_Procuct.ts
+++ abstract_Procuct.ts
@@ -1,18 +1,23 @@
 abstract class Product {
-    id: number,
-    price: number,
-    weight: number,
-    upc: number,
+    id: number;
+    price: number;
+    weight: number;
+    upc: number;
 
-    name: string,
-    type: string,
-    placed: string,
+    name: string;
+    type: string;
+    placed: string;
     shop: string;
 
     constructor(id: number,price: number,weight: number,upc: number,name: string,type: string,placed: string,shop: string) {
         this.id = id;
         this.price = price;
-        this.
+        this.weight = weight;
+        this.upc = upc;
+        this.name = name;
+        this.type = type;
+        this.placed = placed;
+        this.shop = shop;
 
 
     }
