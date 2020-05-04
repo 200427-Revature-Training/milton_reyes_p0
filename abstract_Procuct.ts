d5c01ea3f05d3630e298bd6c8deb27a1f0f5137c
milton-reyes <altf4616@outlook.com>
2020-05-04 14:55:05 -0400

    changes

---
+++ abstract_Procuct.ts
@@ -0,0 +1,21 @@
+abstract class Product {
+    id: number,
+    price: number,
+    weight: number,
+    upc: number,
+
+    name: string,
+    type: string,
+    placed: string,
+    shop: string;
+
+    constructor(id: number,price: number,weight: number,upc: number,name: string,type: string,placed: string,shop: string) {
+        this.id = id;
+        this.price = price;
+        this.
+
+
+    }
+    
+
+}
