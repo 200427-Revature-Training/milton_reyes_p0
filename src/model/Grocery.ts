a4acaff7afd2ce88817005e3a1441067b7cd3d46
milton-reyes <altf4616@outlook.com>
2020-05-04 23:06:58 -0400

    changes to grocery

--- src/model/Grocery.ts
+++ src/model/Grocery.ts
@@ -1,16 +1,23 @@
 class Grocery extends Product {
     id: number;
-    stock: number;
+    quantity: number;
 
     name: string;
+    brand: string;
+    notes: string;
+    category: string;
+    shelflife: string;
 
-    constructor(id: number,stock: number,name: string) {
-        super();
+    constructor(id: number,quantity: number,name: string,brand: string,notes: string,category: string,shelflife: string,price: number,weight: number,upc: number,type: string,placed: string,shop: string) {
+        super(id,price,weight,upc,name,type,placed,shop);
         this.id = id;
-        this.stock = stock;
+        this.quantity = quantity;
 
         this.name = name;
-
+        this.brand = brand;
+        this.notes = notes;
+        this.category = category;
+        this.shelflife = shelflife;
     }
 
 }
