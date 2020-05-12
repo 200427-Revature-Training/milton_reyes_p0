d377570958272d986ae509df7b6eec53530730d1
milton-reyes <altf4616@outlook.com>
2020-05-12 15:35:03 -0400

    added app.ts

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+//import bodyParser from 'body-parser';
+import { db } from './dao/db';
+import { brandRouter } from './router/brandRouter';
+
+const app = express();
+
+const port = process.env.port || 3000;
+app.set('port', port);
+
+/*
+    ? Middleware Registration
+*/
+//app.use(bodyParser.json());
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+/*
+    ? Router Registration
+*/
+app.use('/brands', brandRouter);
+
+process.on('unhandledRejection', () => {
+    db.end().then(() => {
+        console.log('Database pool closed');
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Home app running at http://localhost:${port}`);
+});
